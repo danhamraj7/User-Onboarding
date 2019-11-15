261787c5e11039e9b5e0a0af3b759a4924483663
danhamraj <danhamraj@gmail.com>
2019-11-14 19:15:07 -0500

    Created form with formik, verify with yup and post with axios

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+import UserForm from "./UserForm";
+
+function App() {
+  return (
+    <div className="card">
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
